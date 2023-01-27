c70f7ac5f61b8321dc79fdc63445918b3290572f
sachin <bansodes12@gmail.com>
2023-01-27 22:45:21 +0530

    two space added in base class

---
+++ src/main/java/____NeoStoxBase/base.java
@@ -0,0 +1,29 @@
+package ____NeoStoxBase;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+
+import ____neoStoxUtility.Utility;
+
+
+
+public class base {
+
+	protected static WebDriver driver;
+	public void launch_Browser() throws IOException
+	{
+		driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		//driver.get("https://neostox.com/");
+		
+		driver.get(Utility.readDataFromPropertyFile("url"));
+		Utility.implicitWait(1000, driver);
+		Reporter.log("launching browser", true);
+		
+	}
+
+}
